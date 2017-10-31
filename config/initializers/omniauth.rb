fceef0c4866d7316d3a24daccb7dc7eed4168a8d
cbrennan31 <31743841+cbrennan31@users.noreply.github.com>
2017-10-31 18:51:18 -0400

    more

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, "247560397540-aotsaam36on21hcpjevlitqh0cr9be60.apps.googleusercontent.com", "roVHMOZlP1YKw-Em9p2JtwaZ", {
     :name => "google",
-    :scope => ['contacts','plus.login','plus.me','email','profile'],
+    :scope => ['email','profile'],
     :prompt => "select_account",
     :image_aspect_ratio => "square",
     :image_size => 50,
