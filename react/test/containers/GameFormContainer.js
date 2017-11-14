b0afd6bc716976e351c3ce1e2f44ad899bd5d70c
cbrennan31 <31743841+cbrennan31@users.noreply.github.com>
2017-11-14 14:31:06 -0500

    ignore node modules

--- react/test/containers/GameFormContainer.js
+++ react/test/containers/GameFormContainer.js
@@ -1,6 +1,8 @@
 import GameFormContainer from '../../src/containers/GameFormContainer.js'
 import GameForm from '../../src/components/GameForm.js'
 import ClueFormContainer from '../../src/containers/ClueFormContainer.js'
+import ReactRadioButtonGroup from 'react-radio-button-group'
+import ClueForm from '../../src/components/ClueForm.js'
 
 describe('GameFormContainer', () => {
   let wrapper;
@@ -54,25 +56,86 @@ describe('GameFormContainer', () => {
 
   it ('should trigger the handle change function when the title is changed', () => {
     let gameForm = wrapper.find(GameForm).dive()
-    let titleInput = gameForm.find('input').at(0)
+    let titleInput = gameForm.find('input')
     titleInput.simulate('change', {target: {value: "New Game", name: 'title'}})
+
     expect(GameFormContainer.prototype.handleChange).toHaveBeenCalled()
   })
 
   it ('should trigger the handle change function when the description is changed', () => {
+    let gameForm = wrapper.find(GameForm).dive()
+    let descInput = gameForm.find('textarea')
+    descInput.simulate('change', {target: {value: "New Description", name: 'description'}})
 
+    expect(GameFormContainer.prototype.handleChange).toHaveBeenCalled()
   })
 
   it ('should trigger the handle change function when the strikes are changed', () => {
+    let gameForm = wrapper.find(GameForm).dive()
+    let strikes = gameForm.find(ReactRadioButtonGroup)
+    strikes.simulate('change', '2')
 
+    expect(GameFormContainer.prototype.handleChange).toHaveBeenCalled()
   })
 
   describe('handleChange', () => {
-    it ('should get the state of the title to the value of the event target', () => {
+    it ('should set the state of the title to the value of the event target', () => {
       let gameForm = wrapper.find(GameForm).dive()
-      let titleInput = gameForm.find('input').at(0)
+      let titleInput = gameForm.find('input')
       titleInput.simulate('change', {target: {value: "New Game", name: 'title'}})
+
       expect(wrapper.state().title).toEqual("New Game")
     })
+
+    it ('should set the state of the description to the value of the event target', () => {
+      let gameForm = wrapper.find(GameForm).dive()
+      let descInput = gameForm.find('textarea')
+      descInput.simulate('change', {target: {value: "New Description", name: 'description'}})
+
+      expect(wrapper.state().description).toEqual("New Description")
+    })
+
+    it ('should set the state of strikes to the value of the argument', () => {
+      let gameForm = wrapper.find(GameForm).dive()
+      let strikes = gameForm.find(ReactRadioButtonGroup)
+      strikes.simulate('change', '2')
+
+      expect(wrapper.state().strikes).toEqual('2')
+    })
+  })
+
+  it('should trigger the openClueForm function when the addClue button is clicked', () => {
+    let addClue = wrapper.findWhere(div => div.prop('className') == "add-new-question")
+    addClue.simulate('click')
+
+    expect(GameFormContainer.prototype.openClueForm).toHaveBeenCalled()
+  })
+
+  describe('openClueForm', () => {
+    beforeEach(() => {
+      let addClue = wrapper.findWhere(div => div.prop('className') == "add-new-question")
+      addClue.simulate('click')
+    })
+    it('should increase the number of clueFormsDisplayed in state by 1', () => {
+      expect(wrapper.state().clueFormsDisplayed).toEqual(1)
+    })
+
+    it('should render a ClueFormContainer component', () => {
+      expect(wrapper.find(ClueFormContainer).length).toEqual(1)
+    })
+  })
+
+  it('should trigger the handleClueChange function when the value in ClueFormContainer is modified', () => {
+    let addClue = wrapper.findWhere(div => div.prop('className') == "add-new-question")
+    addClue.simulate('click')
+    let clueFormContainer = wrapper.find(ClueFormContainer).dive()
+    clueFormContainer.simulate('handleClueChange')
+    spyOn(ClueFormContainer.prototype, 'handleChange').and.callThrough()
+    //
+    let clueForm = clueFormContainer.find(ClueForm).dive()
+    clueForm.simulate('change', {target: {value: 400}})
+    //
+    // expect(clueFormContainer.props().handleClueChange).toHaveBeenCalled()
+    expect(GameFormContainer.prototype.handleClueChange).toHaveBeenCalled()
   })
 })
