7617fc1ed6c8e08909f0c5caeb55867b4c2fc6c8
cbrennan31 <31743841+cbrennan31@users.noreply.github.com>
2017-11-24 18:31:27 -0500

    make game view responsive

--- react/src/components/Guidelines.js
+++ react/src/components/Guidelines.js
@@ -4,29 +4,29 @@ const Guidelines = props => {
   return(
     <div className = "text-left">
       <br/>
-      <h6><b>The Basics</b></h6>
+      <h6 className="guidelines-subheader"><b>The Basics</b></h6>
 
       <ul>
-        <li>Choose a category to reveal the question (classic mode only).
+        <li className = "guidelines-item">Choose a category to reveal the question (classic mode only).
           Submit a response and move up to the next score level.
         </li>
 
-        <li>
+        <li className = "guidelines-item">
           Missing a question earns a strike, and reaching the strikes limit ends the game - but you can always play again with new questions!
         </li>
       </ul>
 
-      <h6><b>Responses</b></h6>
+      <h6 className="guidelines-subheader"><b>Responses</b></h6>
       <ul>
-        <li>
+        <li className = "guidelines-item">
           Type in an exact response, including a first and last name for people.
         </li>
 
-        <li>
+        <li className = "guidelines-item">
           Responses do not need to be in question form - just state the answer!
         </li>
 
-        <li>
+        <li className = "guidelines-item">
           Answers are not case-sensitive, and articles ("a," "an," "the") are optional.
         </li>
       </ul>
